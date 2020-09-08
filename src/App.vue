2fdc9418eef5254bf9410ab4c1af39be2c6e4708
Md Shayon <mdshayon0@gmail.com>
2020-09-08 05:28:57 -1200

    using props

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,63 @@
 <template>
+<!-- THIS APP ID IS USING IN MAIN.JS TO MOUNT ALL ELEMENT OR COMPONENT TO HTML -->
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- TO RENDER ANY DYNAMIC VALUE WE NEED TO GIVE TWO CURLY BRACES / 2ND BRACKETS -->
+    <!-- Updates the element’s textContent. If you need to update the part of textContent, you should use {{ Mustache }} interpolations. https://vuejs.org/v2/guide/syntax.html -->
+    <!-- USING DIFFERENT TYPES OF DIRECTIVES https://vuejs.org/v2/api/#v-html -->
+    <!-- Dynamically bind one or more attributes, or a component prop to an expression. https://vuejs.org/v2/api/#v-bind -->
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos
+  },
+  data(){
+    // DATA IS A FUNCTION THAT RETURN AN OBJECT
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "todo one",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "todo two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "todo three",
+          completed: false
+        },
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+
+}
+body{
+  font-family: Arial, Helvetica, sans-serif;
+  line-height:1.4;
+  height: 100%;
+  width:100%;
+  background-image:url(https://images.pexels.com/photos/258109/pexels-photo-258109.jpeg);
+  background-repeat:no-repeat;
+  background-size:cover;
+  background-position:center;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="todo-item" v-bind:class="{'is-complete':todo.completed}" >
+<!-- THIS IS THE PROPS WE ARE GETTING FROM TODOS.VUE -->
+<p>{{todo.title}}</p>
+</div>
+</template>
+
+
+
+<script>
+
+export default{
+    // BY THIS NAME WE WILL IMPORT IN APP.VUE
+    name:"TodoItem",
+    props: ['todo'] // THIS IS THE PROPS WE ARE GETTING FROM TODOS.VUE
+}
+</script>
+
+
+<style scoped>
+p{
+    font-size: 1rem;
+    color: blue;
+}
+
+  .is-complete {
+    text-decoration: line-through;
+  }
+  .del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+</style>
